ee18a7254ada1dcce5de88508733fe3edb5644db
anitazaldivar <128723674+anitazaldivar@users.noreply.github.com>
2024-04-04 01:27:41 +0000

    new class

--- count_the.rb
+++ count_the.rb
@@ -5,3 +5,7 @@ sentences = [
 ]
 sentence = sentences.sample
 # write your program below
+sentence = sentence.gsub(/[^a-z0-9\s]/i, "")
+
+number = sentence.split.length - sentence.gsub("the", "").split.length
+pp  "'the' appeared #{number} times"
